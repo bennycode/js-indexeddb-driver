825024841e4ad9f92b7e1c5d7bb7be478c34866f
Benny Neugebauer <bn@bennyn.de>
2016-01-30 21:11:25 +0100

    Playing around with IndexedDB

--- src/main/js/de/bennyn/storage/indexeddb/Driver.js
+++ src/main/js/de/bennyn/storage/indexeddb/Driver.js
@@ -10,6 +10,43 @@ window.de.bennyn.storage.indexeddb.Driver = function (database, name) {
   };
 };
 
+/**
+ * 
+ * @param {object} schema Database comfiguration including object store configuration
+ * @param {number} schema.version Version of database schema
+ * @param {object} schema.objectStores Object store configuration
+ * @param {string} schema.name Database name
+ * @param {function} createSuccess Success callback function
+ * @param {function} createFailed Error callback function
+ * @returns {undefined}
+ */
+window.de.bennyn.storage.indexeddb.Driver.create = function (schema, createSuccess, createFailed) {
+  try {
+    // IDBOpenDBRequest
+    var request = window.indexedDB.open(schema.name, schema.version);
+  } catch (error) {
+    createFailed(error);
+  }
+
+  request.onsuccess = function (event) {
+    createSuccess(new window.de.bennyn.storage.indexeddb.Driver(request.result, schema.name));
+  };
+
+  request.onerror = request.onblocked = function (event) {
+    createFailed(event.target);
+  };
+
+  request.onupgradeneeded = function (event) {
+    var database = event.target.result;
+    for (var name in schema.objectStores) {
+      if (!database.objectStoreNames.contains(name)) {
+        var optionalParameters = schema.objectStores[name];
+        request.result.createObjectStore(name, optionalParameters);
+      }
+    }
+  };
+};
+
 window.de.bennyn.storage.indexeddb.Driver.prototype.get = function (collection, key, successCallback, errorCallback) {
   var transaction = this.iDBDatabase.transaction(collection, 'readonly');
   var objectStore = transaction.objectStore(collection);
